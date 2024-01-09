2d42d6bc8b5956ef6aeeee53bcf816dbf91f5f01
vincentstaniaszek <134505290+vincentstaniaszek@users.noreply.github.com>
2024-01-09 21:54:18 +0000

    sort of made shooting work

--- 2d shooter/GUI.py
+++ 2d shooter/GUI.py
@@ -67,16 +67,14 @@ class Character(pygame.sprite.Sprite):
         self.vel = self.vel - self.Gravity
         self.updateY(self.vel)
 
-
     # def jump(self):
-
 
     def death(self):
         dead = True
         print("dead")
 
     def collisionCheck(self):
-        print(self.rect.center)
+        # print(self.rect.center)
         if 594 < self.rect.center[1] < 680 and self.vel >= 0:
             self.Gravity = 0
             self.vel = 0
@@ -86,12 +84,14 @@ class Character(pygame.sprite.Sprite):
             return False
 
 class projectile(pygame.sprite.Sprite):
-    def __init__(self, x, y, Damage, speed):
-        self.x = x
-        self.y = y
+    def __init__(self, center, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.center = center
+        self.x = center[0]
+        self.y = center[1]
         self.speed = speed
         self.img = pygame.image.load("_Idle.png")
-        self.image = pygame.transform.scale_by(self.img)
+        self.image = pygame.transform.scale_by(self.img, 1)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
 
@@ -101,26 +101,53 @@ class projectile(pygame.sprite.Sprite):
             rect = self.img.get_rect()
             rect.center = (self.x, self.y)
             self.rect.center = (self.x, self.y)
+        elif direction == "RIGHT":
+            self.x = self.x + 10 * self.speed
+            rect = self.img.get_rect()
+            rect.center = (self.x, self.y)
+            self.rect.center = (self.x, self.y)
+
+    def hitCheck(self, targetCoords):
+        if self.rect == targetCoords:
+            return True
+        else:
+            return False
+
+
+def shoot(center):
+    print("bang")
+    bullet = projectile(center, 1)
+    bullet.updateX("RIGHT")
+
+
 
 def draw(x1, y1, x2, y2, width):
     pygame.draw.rect(surface, GREEN, (x1, y1, x2, y2), width)
 
 player = Character(200, 200, 5, 100, 1)
+# bullet = projectile(player.rect.center, 4)
 
 Jump = False
+gun = False
 
 clock = pygame.time.Clock()
 while True:
 
     surface.fill((43, 163, 212))
-
     floor = draw(100, 700, 1000, 100, 10)
     player.updateX(moveLeft, moveRight)
+
+    surface.blit(player.image, player.rect)
 
 
     if Jump and player.collisionCheck():
         player.vel = -15
         Jump = False
+
+    if gun:
+        # bullet = projectile(player.rect.center, 1)
+        bullet.updateX("RIGHT")
+        surface.blit(bullet.image, bullet.rect)
 
     player.velocity()
     player.collisionCheck()
@@ -144,6 +171,11 @@ while True:
                 Jump = True
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
+            if event.key == pygame.K_j:
+                # shoot(player.rect.center)
+                bullet = projectile(player.rect.center, 3)
+                gun = True
+
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
@@ -152,9 +184,6 @@ while True:
                 moveRight = False
             if event.key == pygame.K_SPACE:
                 Jump = False
-
-
-    surface.blit(player.image, player.rect)
 
     # pygame.display.update()
     pygame.display.flip()
