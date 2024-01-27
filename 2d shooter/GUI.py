ef3517317e4ee4c54f61550032d0e662c4a6eaa7
vincentstaniaszek <134505290+vincentstaniaszek@users.noreply.github.com>
2024-01-27 17:14:30 +0000

    Fixed background and blits now work. Added spritesheet function and started adding animations .

--- 2d shooter/GUI.py
+++ 2d shooter/GUI.py
@@ -2,27 +2,66 @@ import pygame
 from Game import *
 import sys
 import os
+
+pygame.init()
 pygame.mixer.init()
 pygame.display.init()
 pygame.Surface((1600, 900))
-# colours
+screen = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption("2D shooter")
+# pygame.display.set_mode((1600, 900))
+
+# Colours
 GREEN = (0, 255, 0)
 FPS = 60
 RED = (235, 64, 52)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 # Constants
 # GRAVITY = 1
 
-#Audio
+# Audio
 shoot = pygame.mixer.Sound(os.path.join("audio", "Gun sounds", "9mm-pistol-shot.mp3"))
 walking = pygame.mixer.Sound(os.path.join("audio", "concrete-footsteps.mp3"))
 
-#Backgrounds
-Background1 = pygame.image.load("War.png")
+# Backgrounds
+Background1 = pygame.image.load(os.path.join("Backgrounds", "Background 1", "War.png")).convert_alpha()
+ground = pygame.image.load(os.path.join("Backgrounds", "Background 1", "road.png")).convert_alpha()
+
+# Animations
+player_running = pygame.image.load(os.path.join("Sprites", "Soldier 1", "idle(2).png")).convert_alpha()
 
 
-pygame.init()
-surface = pygame.display.set_mode([1600, 900])
-pygame.display.set_caption("2D shooter")
+def get_image(sheet, frame, width, height, scale, colour):
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
+    image = pygame.transform.scale(image, (width * scale, height * scale))
+    image.set_colorkey(colour)
+
+    return image
+
+animation_list = []
+animation_steps = 7
+
+# for i in range(animation_steps):
+#     get_image(player_running, i, 128, 67, 3, WHITE)
+
+
+# frame_0 = get_image(player_running, 0, 128, 67, 3, WHITE)
+# frame_1 = get_image(player_running, 0, 256, 67, 3, WHITE)
+# frame_2 = get_image(player_running, 0, 384, 67, 3, WHITE)
+# frame_3 = get_image(player_running, 0, 512, 67, 3, WHITE)
+# frame_4 = get_image(player_running, 0, 640, 67, 3, WHITE)
+# frame_5 = get_image(player_running, 0, 768, 67, 3, WHITE)
+# frame_6 = get_image(player_running, 0, 896, 67, 3, WHITE)
+
+def loadBack(level):
+    if level == 1:
+        screen.blit(Background1, (0, 0))
+        screen.blit(ground, (0, -210))
+
+
 moveRight = False
 moveLeft = False
 
@@ -37,7 +76,7 @@ class Character(pygame.sprite.Sprite):
         self.max_health = max_health
         self.health = max_health
         # self.img = pygame.image.load(os.path.join("Sprites","Soldier 1", "Idle.png"))
-        self.img = pygame.image.load("_idle.png")
+        self.img = pygame.image.load("_idle.png").convert_alpha()
         self.image = pygame.transform.scale_by(self.img, scale)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
@@ -46,6 +85,7 @@ class Character(pygame.sprite.Sprite):
         self.vel = 0
         self.direction = "RIGHT"
         self.maxBullets = 3
+        self.state = "idle"
 
     def updateX(self, L, R):
         if L:
@@ -97,8 +137,16 @@ class Character(pygame.sprite.Sprite):
             self.Gravity = self.oGravity
             return False
 
+    def animationManage(self):
+        # if self.state == "idle":
+        for i in range(animation_steps):
+            animation_list.append((get_image(player_running, i, 128, 67, 3, WHITE), (0, 0)))
 
-class projectile(pygame.sprite.Sprite):
+
+
+
+
+class Projectile(pygame.sprite.Sprite):
     def __init__(self, center, speed, direction, colour):
         pygame.sprite.Sprite.__init__(self)
         self.center = center
@@ -107,6 +155,10 @@ class projectile(pygame.sprite.Sprite):
         self.speed = speed
         self.colour = colour
         self.direction = direction
+        # self.img = pygame.image.load("R_bullet.png")
+        # self.image = pygame.transform.scale_by(self.img, scale)
+        # self.rect = self.image.get_rect()
+
         if self.direction == "LEFT":
             self.speed = -self.speed
             self.x = center[0] - 60
@@ -114,17 +166,18 @@ class projectile(pygame.sprite.Sprite):
     def hitCheck(self, targetCoords):
         # print(targetCoords[0])
         # print(self.x)
-        if targetCoords[0] <= self.x <= (targetCoords[2] + targetCoords[0]) :
+        if targetCoords[0] <= self.x <= (targetCoords[2] + targetCoords[0]):
             return True
         else:
             return False
 
     def draw(self):
-        pygame.draw.circle(surface, self.colour, (self.x, self.y), 7)
+        pygame.draw.circle(screen, self.colour, (self.x, self.y), 7)
 
 
 def draw(x1, y1, x2, y2, width):
-    pygame.draw.rect(surface, GREEN, (x1, y1, x2, y2), width)
+    pygame.draw.rect(screen, GREEN, (x1, y1, x2, y2), width)
+
 
 player = Character(200, 200, 5, 100, 1)
 
@@ -146,7 +199,7 @@ while True:
         Jump = False
 
     for bullet in bullets:
-        if bullet.x < 1600 and bullet.x > 0:
+        if 1600 > bullet.x > 0:
             bullet.x += bullet.speed
         else:
             bullets.pop(bullets.index(bullet))
@@ -177,7 +230,7 @@ while True:
                 gun = True
                 pygame.mixer.Sound.play(shoot)
                 if len(bullets) < 5:
-                    bullets.append(projectile(player.rect.center, 15, player.direction, RED))
+                    bullets.append(Projectile(player.rect.center, 15, player.direction, RED))
                     # bullets.append(projectile((800,600), 15, player.direction, RED))
 
         if event.type == pygame.KEYUP:
@@ -195,12 +248,16 @@ while True:
             print("hit")
             bullets.pop(bullets.index(bullet))
 
-    surface.blit(Background1, (0, 0))
+    loadBack(1)
     for bullet in bullets:
         bullet.draw()
-    surface.blit(player.image, player.rect)
+    screen.blit(player.image, player.rect)
+    # screen.blit(frame_0, (0, 0))
+    player.animationManage()
+    for i in range(animation_steps):
+        screen.blit(animation_list[i], (0, 0))
     pygame.display.flip()
     clock.tick(60)
-    # clock.tick()
+    # print(clock.get_fps())
 
 # pygame.quit()
