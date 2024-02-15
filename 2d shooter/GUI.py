7f2c1dfe2b33682a927dd8dbfd646b6c816ad7a8
vincentstaniaszek <134505290+vincentstaniaszek@users.noreply.github.com>
2024-02-15 16:42:18 +0000

    Added game states (main menu screen, options and playing). Added changeable difficulty and added mute button.

--- 2d shooter/GUI.py
+++ 2d shooter/GUI.py
@@ -22,17 +22,33 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 SILVER = (175, 179, 176)
+DARK_GREEN = (27, 41, 27)
+GREY = (46, 45, 45)
 
 # Constants
 FPS = 60
 
+# Variables
+difficulty = "easy"
+
 # Audio
+sound = True
 shoot = pygame.mixer.Sound(os.path.join("audio", "Gun sounds", "9mm-pistol-shot.mp3"))
-pygame.mixer.Sound.set_volume(shoot, 0.3)
 walking = pygame.mixer.Sound(os.path.join("audio", "concrete-footsteps.mp3"))
-pygame.mixer.Sound.set_volume(walking, 0.3)
+
+
+def muted(state):
+    if state:
+        pygame.mixer.Sound.set_volume(shoot, 0.3)
+        pygame.mixer.Sound.set_volume(walking, 0.3)
+    elif not state:
+        print("muted")
+        pygame.mixer.Sound.set_volume(shoot, 0)
+        pygame.mixer.Sound.set_volume(walking, 0)
+
 
 # Backgrounds
+game_state = "menu"
 Background1 = pygame.image.load(os.path.join("Backgrounds", "Background 1", "War.png")).convert_alpha()
 ground = pygame.image.load(os.path.join("Backgrounds", "Background 1", "road.png")).convert_alpha()
 
@@ -48,13 +64,21 @@ player_death_right = pygame.image.load(os.path.join("Sprites", "Soldier 1", "Dea
 player_death_left = pygame.image.load(os.path.join("Sprites", "Soldier 1", "Dead_left.png")).convert_alpha()
 # Enemy
 enemy_idle_right = pygame.image.load(os.path.join("Sprites", "Soldier 2", "idle2.png")).convert_alpha()
-enemy_idle_left = pygame.image.load(os.path.join("Sprites", "Soldier 1", "idle(2)_left.png")).convert_alpha()
+enemy_idle_left = pygame.image.load(os.path.join("Sprites", "Soldier 2", "idle2_left.png")).convert_alpha()
 enemy_shoot_right = pygame.image.load(os.path.join("Sprites", "Soldier 2", "shot_1(2).png")).convert_alpha()
-enemy_shoot_left = pygame.image.load(os.path.join("Sprites", "Soldier 1", "shot_2(1)_left.png")).convert_alpha()
+enemy_shoot_left = pygame.image.load(os.path.join("Sprites", "Soldier 2", "shot_1(2)_left.png")).convert_alpha()
 enemy_walk_right = pygame.image.load(os.path.join("Sprites", "Soldier 2", "Walk2.png")).convert_alpha()
-enemy_walk_left = pygame.image.load(os.path.join("Sprites", "Soldier 1", "Run2_left.png")).convert_alpha()
+enemy_walk_left = pygame.image.load(os.path.join("Sprites", "Soldier 2", "Walk2_left.png")).convert_alpha()
 # enemy_death_right = pygame.image.load(os.path.join("Sprites", "Soldier 2", "Dead_right.png")).convert_alpha()
 # enemy_death_left = pygame.image.load(os.path.join("Sprites", "Soldier 1", "Dead_left.png")).convert_alpha()
+
+
+# buttons
+play_button = (550, 330, 500, 110)
+options_button = (550, 480, 500, 110)
+exit_button = (550, 630, 500, 110)
+mute_button = (550, 330, 500, 110)
+difficulty_button = (550, 480, 500, 110)
 
 update = 100
 cooldown = 750
@@ -93,31 +117,98 @@ class wait():
             return True
 
 
-animation_list = []
-
-
 def loadBack(level):
     if level == 1:
         screen.blit(Background1, (0, 0))
         screen.blit(ground, (0, -210))
 
 
+def mainMenu():
+    screen.fill(DARK_GREEN)
+    pygame.draw.rect(screen, GREY, play_button)
+    pygame.draw.rect(screen, GREY, options_button)
+    pygame.draw.rect(screen, GREY, exit_button)
+    screen.blit(pygame.font.SysFont("Times New Roman", 30).render("LEVEL SELECT", True, WHITE, ), (700, 365))
+    screen.blit(pygame.font.SysFont("Times New Roman", 30).render("OPTIONS", True, WHITE, ), (730, 515))
+    screen.blit(pygame.font.SysFont("Times New Roman", 30).render("EXIT", True, WHITE, ), (765, 665))
+
+
+def menuControls():
+    global game_state
+    global sound
+    global difficulty
+    if game_state == "menu":
+        if event.type == pygame.MOUSEBUTTONDOWN and play_button[0] < pygame.mouse.get_pos()[0] < play_button[
+            0] + 500 and \
+                play_button[1] < pygame.mouse.get_pos()[1] < play_button[1] + 110:
+            game_state = "playing"
+        if event.type == pygame.MOUSEBUTTONDOWN and options_button[0] < pygame.mouse.get_pos()[0] < options_button[
+            0] + 500 and options_button[1] < pygame.mouse.get_pos()[1] < options_button[1] + 110:
+            game_state = "options"
+        if event.type == pygame.MOUSEBUTTONDOWN and exit_button[0] < pygame.mouse.get_pos()[0] < exit_button[
+            0] + 500 and \
+                exit_button[1] < pygame.mouse.get_pos()[1] < exit_button[1] + 110:
+            pygame.quit()
+    elif game_state == "options":
+        if sound and event.type == pygame.MOUSEBUTTONDOWN and mute_button[0] < pygame.mouse.get_pos()[0] < mute_button[
+            0] + 500 and \
+                mute_button[1] < pygame.mouse.get_pos()[1] < mute_button[1] + 110:
+            sound = False
+            muted(sound)
+        elif not sound and event.type == pygame.MOUSEBUTTONDOWN and mute_button[0] < pygame.mouse.get_pos()[0] < \
+                mute_button[
+                    0] + 500 and \
+                mute_button[1] < pygame.mouse.get_pos()[1] < mute_button[1] + 110:
+            sound = True
+            muted(sound)
+        if difficulty == "easy" and event.type == pygame.MOUSEBUTTONDOWN and difficulty_button[0] < \
+                pygame.mouse.get_pos()[0] < difficulty_button[
+            0] + 500 and difficulty_button[1] < pygame.mouse.get_pos()[1] < difficulty_button[1] + 110:
+            difficulty = "hard"
+        elif difficulty == "hard" and event.type == pygame.MOUSEBUTTONDOWN and difficulty_button[0] < \
+                pygame.mouse.get_pos()[0] < difficulty_button[
+            0] + 500 and difficulty_button[1] < pygame.mouse.get_pos()[1] < difficulty_button[1] + 110:
+            difficulty = "easy"
+
+        if event.type == pygame.MOUSEBUTTONDOWN and exit_button[0] < pygame.mouse.get_pos()[0] < exit_button[
+            0] + 500 and \
+                exit_button[1] < pygame.mouse.get_pos()[1] < exit_button[1] + 110:
+            game_state = "menu"
+
+
 moveRight = False
 moveLeft = False
 
 
+def options():
+    global game_state
+    screen.fill(DARK_GREEN)
+    pygame.draw.rect(screen, GREY, mute_button)
+    pygame.draw.rect(screen, GREY, difficulty_button)
+    pygame.draw.rect(screen, GREY, exit_button)
+    if sound:
+        screen.blit(pygame.font.SysFont("Times New Roman", 30).render("MUTE", True, WHITE, ), (760, 365))
+    elif not sound:
+        screen.blit(pygame.font.SysFont("Times New Roman", 30).render("UNMUTE", True, WHITE, ), (740, 365))
+    if difficulty == "easy":
+        screen.blit(pygame.font.SysFont("Times New Roman", 30).render("DIFFICULTY EASY", True, WHITE, ), (680, 515))
+    else:
+        screen.blit(pygame.font.SysFont("Times New Roman", 30).render("DIFFICULTY HARD", True, WHITE, ), (680, 515))
+    screen.blit(pygame.font.SysFont("Times New Roman", 30).render("BACK", True, WHITE, ), (765, 665))
+
+
 class Character(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, max_health, speed):
+    def __init__(self, x, y, max_health, speed):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-        self.scale = scale
+        self.scale = 1
         self.speed = speed
         self.max_health = max_health
         self.health = max_health
         self.img = pygame.image.load(os.path.join("Sprites", "Soldier 1", "Idle.png")).convert_alpha()
         self.img = pygame.image.load("_idle.png").convert_alpha()
-        self.image = pygame.transform.scale_by(self.img, scale)
+        self.image = pygame.transform.scale_by(self.img, self.scale * 5)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
         self.oGravity = -0.6
@@ -219,8 +310,9 @@ class Character(pygame.sprite.Sprite):
 
 
 class Player(Character):
-    def __init__(self, x, y, scale):
-        super().__init__(x, y, scale, 100, 1)
+    def __init__(self, x, y):
+        super().__init__(x, y, 100, 1)
+        self.scale = 3
 
     def UI(self):
         # Health Bar
@@ -253,7 +345,7 @@ class Player(Character):
             animation_list = []
             if self.direction == "RIGHT":
                 for i in range(animation_steps):
-                    animation_list.append(get_image(player_idle_right, i, 128, 67, 3, WHITE))
+                    animation_list.append(get_image(player_idle_right, i, 128, 67, self.scale, WHITE))
                 if self.c_time - self.last_update >= self.animation_cooldown:
                     self.frame += 1
                     if self.frame >= len(animation_list):
@@ -337,12 +429,14 @@ class Player(Character):
 
 
 class Enemy(Character):
-    def __init__(self, x, y, scale):
-        super().__init__(x, y, scale, 100, 0.3)
+    def __init__(self, x, y, ):
+        super().__init__(x, y, 100, 0.3)
+        self.scale = 3
         self.t = pygame.time.get_ticks()
         self.T = self.t + 500
         self.maxBullets = 1
         self.move = random.randint(-1, 1)
+        self.seen = False  # private attribute which outputs True if the player has been seen by the enemy
 
     def healthBar(self):
         pygame.draw.rect(screen, SILVER, (self.x - 45, self.y - 120, 100, 5), 20)
@@ -381,20 +475,20 @@ class Enemy(Character):
             self.updateX(0, 1)
             self.state = "walking"
             self.direction = "RIGHT"
-            print("c", self.c_time)
-            print("u", self.last_update)
-            # print("", )
+            # print("c", self.c_time)
+            # print("u", self.last_update)
+            print("", )
             if self.c_time - self.last_update + random.randint(50, 200) >= 200:
                 self.move = 0
                 self.last_update = self.c_time
-        print(self.move)
+        # print(self.move)
         if self.move == -1:
             self.updateX(1, 0)
             self.state = "walking"
             self.direction = "LEFT"
-            print("c", self.c_time)
-            print("u", self.last_update)
-            # print("", )
+            # print("c", self.c_time)
+            # print("u", self.last_update)
+            print("", )
             if self.c_time - self.last_update >= 100:
                 self.move = 0
                 self.last_update = self.c_time
@@ -408,6 +502,17 @@ class Enemy(Character):
         self.movement()
         if self.vision(player1.x, player1.y):
             enemy.shoot()
+            self.seen = True
+            self.move = 0
+
+        if self.seen:
+            if not self.vision(player1.x, player1.y):
+                if self.x - player1.x < 0:
+                    self.move = 1
+                else:
+                    self.move = -1
+            else:
+                self.move = 0
 
     def animationManage(self):
         self.c_time = pygame.time.get_ticks()
@@ -464,7 +569,7 @@ class Enemy(Character):
                     self.last_update = self.c_time
             if self.direction == "LEFT":
                 for i in range(animation_steps):
-                    animation_list.append(get_image(enemy_walk_left, i, 128, 61, 3, WHITE))
+                    animation_list.append(get_image(enemy_walk_left, i, 128, 65, 3, WHITE))
                 if self.c_time - self.last_update >= self.animation_cooldown:
                     self.frame += 1
                     if self.frame >= len(animation_list):
@@ -497,7 +602,8 @@ class Projectile(pygame.sprite.Sprite):
     def hitCheck(self, targetCoords):
         # print(targetCoords[0])
         # print(self.x)
-        if targetCoords[0] <= self.x <= (targetCoords[2] + targetCoords[0]):
+        if targetCoords[0] <= self.x <= (targetCoords[2] + targetCoords[0]) and targetCoords[1] <= self.y <= (
+                targetCoords[1] + targetCoords[3]):
             return True
         else:
             return False
@@ -511,7 +617,7 @@ def draw(x1, y1, x2, y2, width):
 
 
 delay = wait()
-player1 = Player(200, 200, 5)
+player1 = Player(200, 200)
 
 Jump = False
 
@@ -546,81 +652,91 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                moveLeft = True
-                pygame.mixer.Sound.play(walking)
-                player1.direction = "LEFT"
-                player1.state = "running"
-            if event.key == pygame.K_d:
-                pygame.mixer.Sound.play(walking)
-                moveRight = True
-                player1.state = "running"
-                player1.direction = "RIGHT"
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            if event.key == pygame.K_SPACE:
-                Jump = True
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-            if event.key == pygame.K_j:
-                player1.shoot()
-            if event.key == pygame.K_l:
-                # enemies.append(Enemy(random.randint(30, 1570), 300, 5))
-                enemies.append(Enemy(1000, 300, 5))
-            if event.key == pygame.K_p:
-                player1.health = player1.health - 32
-                # delay.wait1(100)
-                # if time() - start >= WAITING_INTERVAL:
-                #     print("yayay")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                moveLeft = False
-                player1.state = "idle"
-                pygame.mixer.Sound.stop(walking)
-            if event.key == pygame.K_d:
-                moveRight = False
-                player1.state = "idle"
-                pygame.mixer.Sound.stop(walking)
-            if event.key == pygame.K_SPACE:
-                Jump = False
-    loadBack(1)
 
-    # screen.blit(player.image, player.rect)
-    # pygame.draw.rect(screen, RED, player.rect)
+        if game_state != "playing":
+            menuControls()
 
-    player1.animationManage()
-    for enemy in enemies:
-        enemy.velocity()
-        enemy.collisionCheck()
-        enemy.animationManage()
-        enemy.healthBar()
-        enemy.AI()
-        enemy.bulletManage()
-        for bullet in enemy.bullets:
-            if bullet.hitCheck(player1.rect):
-                player1.updateHealth(1)
-                print("hit")
-                enemy.bullets.pop(enemy.bullets.index(bullet))
-        for bullet in player1.bullets:
-            if bullet.hitCheck(enemy.rect):
-                enemy.updateHealth(25)
-                print("hit")
-                player1.ammoVisual.append((1040, -150 - 10 * (len(player1.ammoVisual))))
-                player1.bullets.pop(player1.bullets.index(bullet))
+        if game_state == "playing":
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    moveLeft = True
+                    pygame.mixer.Sound.play(walking)
+                    player1.direction = "LEFT"
+                    player1.state = "running"
+                if event.key == pygame.K_d:
+                    pygame.mixer.Sound.play(walking)
+                    moveRight = True
+                    player1.state = "running"
+                    player1.direction = "RIGHT"
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_SPACE:
+                    Jump = True
 
-        if enemy.getHealth() <= 0:
-            if enemy.c_time - update1 >= 2000:
-                enemies.pop(enemies.index(enemy))
+                if event.key == pygame.K_j:
+                    player1.shoot()
+                if event.key == pygame.K_l:
+                    # enemies.append(Enemy(random.randint(30, 1570), 300, 5))
+                    enemies.append(Enemy(1000, 300))
+                if event.key == pygame.K_p:
+                    game_state = "options"
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    moveLeft = False
+                    player1.state = "idle"
+                    pygame.mixer.Sound.stop(walking)
+                if event.key == pygame.K_d:
+                    moveRight = False
+                    player1.state = "idle"
+                    pygame.mixer.Sound.stop(walking)
+                if event.key == pygame.K_SPACE:
+                    Jump = False
 
+    if game_state == "menu":
+        mainMenu()
+    if game_state == "options":
+        options()
+
+    # pygame.draw.rect(screen, RED, player1.rect)
+    if game_state == "playing":
+        loadBack(1)
+        player1.animationManage()
+        for enemy in enemies:
+            enemy.velocity()
+            enemy.collisionCheck()
+            enemy.animationManage()
+            enemy.healthBar()
+            enemy.AI()
+            enemy.bulletManage()
+            for bullet in enemy.bullets:
+                if bullet.hitCheck(player1.rect):
+                    if difficulty == "easy":
+                        player1.updateHealth(15)
+                    elif difficulty == "hard":
+                        player1.updateHealth(22)
+                    print("hit")
+                    enemy.bullets.pop(enemy.bullets.index(bullet))
+            for bullet in player1.bullets:
+                if bullet.hitCheck(enemy.rect):
+                    enemy.updateHealth(25)
+                    print("hit")
+                    player1.ammoVisual.append((1040, -150 - 10 * (len(player1.ammoVisual))))
+                    player1.bullets.pop(player1.bullets.index(bullet))
+
+            if enemy.getHealth() <= 0:
+                if enemy.c_time - update1 >= 2000:
+                    enemies.pop(enemies.index(enemy))
+        player1.UI()
+        player1.bulletManage()
     # pygame.time.set_timer()
     # player1.healthBar()
     # player1.ammoCounter()
     # print(player1.x)
     # print(player1.rect)
-    player1.UI()
-    player1.bulletManage()
+
     pygame.display.flip()
     clock.tick(60)
     # print(clock.get_fps())
--- 2d shooter/spritesheets.py
+++ 2d shooter/spritesheets.py
@@ -1,18 +1,18 @@
-from GUI import *
-import pygame
-# pygame.init()
-
-BLACK = (0, 0, 0)
-
-player_running = pygame.image.load(os.path.join("Sprites", "Soldier 1", "idle.png")).convert_alpha()
-
-
-def get_image(sheet, frame, width, height, scale, colour):
-    image = pygame.Surface((width, height)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(colour)
-
-    return image
-
-frame_0 = get_image(player_running, 0, 128, 128, 3, BLACK)
+# from GUI import *
+# import pygame
+# # pygame.init()
+#
+# BLACK = (0, 0, 0)
+#
+# player_running = pygame.image.load(os.path.join("Sprites", "Soldier 1", "idle.png")).convert_alpha()
+#
+#
+# def get_image(sheet, frame, width, height, scale, colour):
+#     image = pygame.Surface((width, height)).convert_alpha()
+#     image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
+#     image = pygame.transform.scale(image, (width * scale, height * scale))
+#     image.set_colorkey(colour)
+#
+#     return image
+#
+# frame_0 = get_image(player_running, 0, 128, 128, 3, BLACK)
